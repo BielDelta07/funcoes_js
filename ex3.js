b415c94a2ea22e7f011c21701b25c002314b8c53
Delta <gabrieldavet07@gmail.com>
2026-05-15 11:25:31 -0300

    Delete ex3.js

--- ex3.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let nota_final = Number(prompt("Digite a sua nota final: "));
-
-if(nota_final >= 7){
-    console.log("Você está aprovado ;)");
-}else{
-    console.log("Você está reprovado :(");
-}
