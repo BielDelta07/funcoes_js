e81c3266e18fbaaeba70c35f1b02492ad8c6e437
Delta <gabrieldavet07@gmail.com>
2026-05-15 11:25:57 -0300

    Delete ex6.js

--- ex6.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let soma = 0;
-
-for(let i = 1; i <= 100; i++){
-    soma = soma + i;
-    console.log("Resultado: " + soma);
-}
-console.log("A soma de todos os números de 1 até 100 é: " + soma);
